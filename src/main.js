2f271007dafcfc4ca551e20a9b64a5d59120ffad
qxxxoDev <tigran.qxxxo.dev@gmail.com>
2021-08-22 17:43:00 +0300

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import { startServer } from './server/server.js'
-import bot from './bot/bot.js'
-
 console.log('App started!')
 
+/* import { startServer } from './server/server.js'
+import bot from './bot/bot.js'
+
 // Start server for a bot
-startServer(bot)
+startServer(bot) */
