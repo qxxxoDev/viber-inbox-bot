430d550f7e1cd62bb50cd06298158e56341e67a1
qxxxoDev <tigran.qxxxo.dev@gmail.com>
2021-08-24 21:15:23 +0300

    Make inifinite connection

--- src/mail/client.js
+++ src/mail/client.js
@@ -4,48 +4,51 @@ const { createImap } = require('./imap.js')
 const { EMAIL, PASSWORD, IMAP_HOST } = require('../config.js')
 
 
-const imap = createImap({
-    user: EMAIL,
-    password: PASSWORD,
-    host: IMAP_HOST
-})
-
-const openInbox = cb => imap.openBox('INBOX', true, cb)
-
-imap.once('ready', () => openInbox((err, box) => {
-    console.log('Imap is ready!')
-    if (err) console.log(err)
-
-    imap.on('mail', count => {
-
-        const from = box.messages.total - count + 1
-
-        const f = imap.seq.fetch(from + ':*', {
-            bodies: '',
-            struct: true
-        })
-
-        f.on('message', msg => {
-            msg.on('body', stream => {
-                simpleParser(stream, (err, mail) => {
-                    if (err) throw err
-                    emitMailReceivedEvent(mail)
-                })
-            })
-        })
-
+const connectToMailServer = () => {
+    const imap = createImap({
+        user: EMAIL,
+        password: PASSWORD,
+        host: IMAP_HOST
     })
 
-}))
+    const openInbox = cb => imap.openBox('INBOX', true, cb)
 
-imap.once('error', function(err) {
-  console.log(err)
-})
+    imap.once('ready', () => openInbox((err, box) => {
+        if (err) throw err
+        console.log('Imap connected!')
 
-imap.once('end', function() {
-  console.log('Imap connection ended');
-})
+        imap.on('mail', count => {
 
-const connectToMailServer = () => imap.connect()
+            const from = box.messages.total - count + 1
+
+            const f = imap.seq.fetch(from + ':*', {
+                bodies: '',
+                struct: true
+            })
+
+            f.on('message', msg => {
+                msg.on('body', stream => {
+                    simpleParser(stream, (err, mail) => {
+                        if (err) throw err
+                        emitMailReceivedEvent(mail)
+                    })
+                })
+            })
+
+        })
+
+    }))
+
+    imap.once('error', function(err) {
+        throw err
+    })
+
+    imap.once('end', function() {
+        console.log('Imap connection ended.\nReconnectiong...');
+        connectToMailServer()
+    })
+
+    imap.connect()
+}
 
 module.exports = { connectToMailServer }
