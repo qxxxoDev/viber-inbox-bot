1c9fc45cea5c53f0a9ee11663d28b916eb1acb39
qxxxoDev <tigran.qxxxo.dev@gmail.com>
2021-08-24 17:38:27 +0300

    jui

--- src/bot/logic.js
+++ src/bot/logic.js
@@ -86,7 +86,7 @@ const useBotLogic = bot => {
             }
 
             await setUser(userProfile)
-            
+            console.log(isSubscribed)
             if (!isSubscribed){
                 const START_MESSAGE = new TextMessage(`Hi, ${userProfile.name}!\nNice to meet you!`, ...useKeyboard(START_KEYBOARD))
                 bot.sendMessage(userProfile, START_MESSAGE)
